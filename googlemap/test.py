e3346fe4fd99be84ddd50ed90e38a3bd822f7560
lbfacto <48971071+lbfacto@users.noreply.github.com>
2022-12-26 16:16:15 +0000

    Add files via upload

---
+++ googlemap/test.py
@@ -0,0 +1,32 @@
+import  sys, webbrowser, pyperclip
+
+
+
+if len(sys.argv)>1:
+    add=''.join(sys.argv[1:])
+else:
+    add=pyperclip.paste()
+    
+webbrowser.open('www.google.com/maps/search/?api=1&query')
+
+
+
+#AIzaSyCeH5-kL_eBZ_e8IFm8IOxC0XuL2Ud6LJ8
+#AIzaSyBtR1m7EdO5_sFOzNTOY30w-SdwvdtgA8o
+#AIzaSyB8EK2SO3xUE-2RjK7pyxlPJee_kdoKTXY
+#AIzaSyCBBXKOGCWl89l2SXr5YGZa5iWOPoNLMJQ
+#https://console.cloud.google.com/apis/credentials/key/af66a0cb-927a-4533-8160-8b39c590c1e7?hl=fr&project=prefab-envoy-316021
+
+
+
+
+
+
+
+#<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-121.64062500000001%2C-43.83452678223683%2C59.41406250000001%2C60.06484046010452&amp;layer=mapnik&amp;marker=13.239945499286312%2C-31.11328125" style="border: 1px solid black">
+#</iframe>
+#<br/>
+#<small>
+#<a href="https://www.openstreetmap.org/?mlat=13.24&amp;mlon=-31.11#map=3/13.24/-31.11">Afficher une carte plus grande</a>
+#</small>
+#https://www.openstreetmap.org/?mlat=14.7455&mlon=-17.1963#map=10/14.7455/-17.1963
